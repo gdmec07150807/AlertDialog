5bb34e3f28f9842a4131c19ecd7189e9e0e5e4ca
07150807 <497115679@qq.com>
2016-10-10 22:13:23 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/c07150807/work5/Main2Activity.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.c07150807.work5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
